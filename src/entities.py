e7079c01329cb478a585305521791830b916bc0a
Iconejey <43481095+Iconejey@users.noreply.github.com>
2020-11-10 16:56:40 -0400

    V1.0
    - Déplacement
    - Menu
    - Etoiles

--- src/entities.py
+++ src/entities.py
@@ -1,10 +1,13 @@
 import pygame as pg
+from math import atan2, pi
+from random import random
 
 class Entity:
     def __init__(self, pos: tuple, img_bnk: dict, act_img: str):
         self.x, self.y = pos
         self.img_bnk = img_bnk
         self.act_img = act_img
+        self.body_angle = 0
 
     @staticmethod
     def rotate(img: pg.Surface, angle: float = 0):
@@ -14,7 +17,145 @@ class Entity:
 
         return rot_img, new_rect
 
-    def draw(self, s: pg.Surface, angle: float = 0, scale: float = 1) -> None:
-        img = self.img_bnk[self.act_img]
+    def show(self, s: pg.Surface, scale: float = 1):
+        self.draw(s, None, self.body_angle, scale)
+
+    def draw(self, s: pg.Surface, img_name: str = None, angle: float = 0, scale: float = 1) -> None:
+        if img_name is None:
+            img_name = self.act_img
+
+        img = self.img_bnk[img_name]
         img = pg.transform.scale(img, [img.get_width() * scale, img.get_height() * scale])
-        s.blit(*Entity.rotate(img, angle))
+        img, rect = Entity.rotate(img, angle)
+        rect.x += self.x - 24 * scale
+        rect.y += self.y - 24 * scale
+        s.blit(img, rect)
+
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Ship(Entity):
+    def __init__(self, pos: tuple, img_bnk: dict):
+        super(Ship, self).__init__(pos, img_bnk, 'ship_0')
+        self.level = 0
+        self.level_frame = 0
+        self.flame = 0
+        self.flame_left = True
+        self.flame_right = True
+        self.body_angle = 0
+        self.weapon_angle = 0
+        self.dir = [0, -1]
+        self.aim = [0, -100]
+        self.vel = [0, 0]
+
+    def animate(self, time: int):
+        if time % 3 == 0:
+            if self.level_frame < self.level * 5:
+                self.level_frame += 1
+
+            self.flame += 1
+            if self.flame > 4:
+                self.flame = 0
+
+        if time > 200 and time % 60 == 0 and self.level < 2:
+            self.level += 1
+
+    def show(self, s: pg.Surface, scale: float = 1):
+        self.draw(s, 'ship_' + str(self.level_frame), self.body_angle, scale)
+        self.draw(s, 'weapon_' + str(self.level_frame), self.weapon_angle, scale)
+
+        if self.flame_right:
+            self.draw(s, 'flame_R_' + str(self.level * 5 + self.flame), self.body_angle, scale)
+        if self.flame_left:
+            self.draw(s, 'flame_L_' + str(self.level * 5 + self.flame), self.body_angle, scale)
+
+    def move(self, dx, dy):
+        if dx or dy:
+            n = 1.4 if (dx and dy) else 2
+            self.dir = [n * dx, n * dy]
+
+        self.flame_left = False
+        self.flame_right = False
+
+        angle =  atan2(self.dir[0], self.dir[1]) * 180 / pi - 180
+
+        if abs(angle - self.body_angle) > 180:
+            if self.body_angle > angle:
+                self.body_angle -= 360
+            else:
+                self.body_angle += 360
+
+        if self.level_frame == self.level * 5:
+            if angle > self.body_angle:
+                self.flame_right = True
+                self.body_angle += 10
+                self.weapon_angle += 10
+
+            if angle < self.body_angle:
+                self.flame_left = True
+                self.body_angle -= 10
+                self.weapon_angle -= 10
+
+            if self.flame_left and self.flame_right:
+                self.flame_left = False
+                self.flame_right = False
+
+            if (dx or dy) and abs(angle - self.body_angle) < 10:
+                self.flame_left = True
+                self.flame_right = True
+                self.vel[0] += self.dir[0]
+                self.vel[1] += self.dir[1]
+
+        self.vel[0] *= 0.95
+        self.vel[1] *= 0.95
+
+        self.x += self.vel[0]
+        self.y += self.vel[1]
+
+    def aimTo(self, x, y):
+        self.aim = [x - self.x, y - self.y]
+
+        angle =  atan2(self.aim[0], self.aim[1]) * 180 / pi - 180
+
+        if abs(angle - self.weapon_angle) > 180:
+            if self.weapon_angle > angle:
+                self.weapon_angle -= 360
+            else:
+                self.weapon_angle += 360
+
+        if abs(angle - self.weapon_angle) < 9:
+            self.weapon_angle = angle
+        else:
+            if angle > self.weapon_angle:
+                self.weapon_angle += 9
+
+            if angle < self.weapon_angle:
+                self.weapon_angle -= 9
+
+class Star:
+    def __init__(self, W: int, H: int, scale):
+        self.limits = W, H
+        self.x = 0
+        self.y = 0
+        self.size = 1
+        self.scale = scale
+        self.rand_time = 0
+
+    def rand(self):
+        W, H = self.limits
+        self.x = int(random() * W)
+        self.y = int(random() * (H + 100)) - 100
+        self.size = random() * self.scale * 1.2
+        self.rand_time += random() * 100
+
+    def move(self, time):
+        self.y += self.size
+        if time > self.rand_time:
+            self.rand()
+
+
+    def show(self, s: pg.Surface):
+        pg.draw.rect(s, [200, 200, 200], [self.x, self.y, self.size, self.size])
+        
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import os, platform
-from entities import Entity
+from entities import Entity, Ship, Star
+from math import sin
 
 def getImgDict(path: str) -> dict:
 	d = {}
@@ -20,31 +21,64 @@ if __name__ == "__main__":
 		windll.shcore.SetProcessDpiAwareness(1)
 
 	pg.init()
+	clock = pg.time.Clock()
 
-	SCREEN_W, SCREEN_H = 900, 600
+	SCREEN_W, SCREEN_H = 1500, 900
 	SCREEN = pg.display.set_mode((SCREEN_W, SCREEN_H))
 	SCALE = 3
 
 	images = getImgDict('../img')
-	
-	player = Entity([100, 100], images['player'], 'ship_0')
-	print(player.x, player.y)
 
-	mode = 'game'
+	font = pg.font.Font('../Consolas.ttf', 24)
+	bfont = pg.font.Font('../Consolas.ttf', 48)
+	bfont.set_bold(True)
+	
+	player = Ship([SCREEN_W // 2, SCREEN_H // 2], images['player'])
+	stars = [Star(SCREEN_W, SCREEN_H, SCALE) for i in range(100)]
+
+	mode = 'menu'
 	time = 0
 	while mode:
 		time += 1
+		clock.tick(30)
+		
 		for e in pg.event.get():
 			if e.type == pg.QUIT:
 				mode = None
 				continue
 
 		keys_press = pg.key.get_pressed()
-		mouse_pess = pg.mouse.get_pressed()
+		mouse_press = pg.mouse.get_pressed()
 		mouse_pos = pg.mouse.get_pos()
 
 		SCREEN.fill([0, 0, 0])
 
-		player.draw(SCREEN, time / 10, SCALE)
+		for star in stars:
+			star.move(time)
+			star.show(SCREEN)
+
+		direction = [0, 0]
+
+		if mode == 'menu':
+			shoot_moop = bfont.render('Shoot Moop', False, [255, 255, 255, 128])
+			SCREEN.blit(shoot_moop, [SCREEN_W // 2 - shoot_moop.get_size()[0] // 2, SCREEN_H // 4])
+
+			press_start = font.render('Press [Enter] to start.', False, [255, 255, 255, abs(255 * sin(time/100))])
+			SCREEN.blit(press_start, [SCREEN_W // 2 - press_start.get_size()[0] // 2, SCREEN_H * 3 // 4])
+
+			if keys_press[pg.K_RETURN]:
+				mode = 'game'
+			
+		if mode == 'game':
+			if keys_press[pg.K_w]: direction[1] -= 1
+			if keys_press[pg.K_s]: direction[1] += 1
+			if keys_press[pg.K_a]: direction[0] -= 1
+			if keys_press[pg.K_d]: direction[0] += 1
+
+			player.aimTo(*mouse_pos)
+
+		player.move(*direction)
+		player.animate(time)
+		player.show(SCREEN, SCALE)
 		
 		pg.display.update()
